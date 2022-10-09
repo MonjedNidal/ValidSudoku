8ff1b2aa5cf27fd6f966dbc35833cb8650b063ba
monjednidal <monjed1010@gamil.com>
2022-10-09 16:21:12 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,36 @@
-import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         char[][] board =
-        {{'5','3','.','.','7','.','.','.','.'},
-        {'6','.','.','1','9','5','.','.','.'},
-        {'.','9','8','.','.','.','.','6','.'},
-        {'8','.','.','.','6','.','.','.','3'},
-        {'4','.','.','8','.','3','.','.','1'},
-        {'7','.','.','.','2','.','.','.','6'},
-        {'.','6','.','.','.','.','2','8','.'},
-        {'.','.','.','4','1','9','.','.','5'},
-        {'.','.','.','.','8','.','.','7','9'}};
+        {{'.','2','.','.','.','5','.','.','.'},
+        {'.','.','.','.','.','4','.','.','.'},
+        {'.','.','.','9','.','.','7','.','.'},
+        {'.','.','8','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.','.'},
+        {'.','.','.','.','7','4','3','.','9'},
+        {'.','.','.','.','.','.','9','.','.'},
+        {'.','7','.','.','6','.','.','.','5'},
+        {'3','.','.','7','.','.','.','.','.'}};// this isn't valid sudoku because the number 4 in the second row appears another time in the same column.
 
         System.out.println(isValidSudoku(board));
     }
     public static boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for (int i = 0; i < 9; i++) {// loop over all rows
+            for (int j = 0; j < 9; j++) {// loop over all columns
                 if (board[i][j] != '.'){ // it's a number
-                        int l = (i + 1 == board.length)? i : i+1;
-                        for (int k = (j + 1 == board.length)? j : j+1; k < 9; k++) {
-                            if (board[i][j]  == board[i][k] && k != j) {
+                    for (int k = (j + 1 == board.length)? j : j+1; k < 9; k++) {// check the columns of the number if there is any duplicate return false
+                        if (board[i][j]  == board[i][k] && k != j) {
                                 return false;
-                            } else if (board[i][j] == board[l][j] && l != i+1) {
-                                return false;
-                            }
-                                l = (l%8) + 1;
                         }
+                    }
+                    for (int k = (i + 1 == board.length)? i : i+1; k < 9; k++) {// check the rows of the number if there is any duplicate return false
+                        System.out.print(board[k][j]);
+                        if (board[i][j]  == board[k][j] && k != i) {
+                            return false;
+                        }
+                    }
                 }
             }
         }return isValidSquares(board);
@@ -41,19 +42,17 @@ public class Main {
 
                 for (int k = i; k < i+3; k++) {
                     for (int l = j; l < j+3; l++) {
-                        if (board[k][l] != '.'){
+                        if (board[k][l] != '.'){// check if the char is a number.
                             if (map.isEmpty()){
                                 map.put(board[k][l], 1);
-                            }else if (map.containsKey(board[k][l])){
+                            }else if (map.containsKey(board[k][l])){// here means if the same number saw another time in the same 3 x 3 square return false.
                                 return false;
                             }else{
-                                map.put(board[k][l], 1);
+                                map.put(board[k][l], 1); // if the map isn't empty and the number isnt key put it on the map.
                             }
                         }
                     }
-                    System.out.println(map.keySet());
                 }
-
             }
         }return true;
     }
